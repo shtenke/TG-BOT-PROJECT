2f6f16fde751c2afe45c09e7473b476279d5dffa
shtenke <shtenke.ps@mail.ru>
2025-03-29 15:17:33 +0300

    beta1

---
+++ bot.py
@@ -0,0 +1,32 @@
+import telebot
+from config import TOKEN
+import time
+from logic import CareerAdvisorBot
+
+
+bot = telebot.TeleBot(TOKEN)
+TG = CareerAdvisorBot()
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    time.sleep(2)
+    bot.send_message(message.chat.id,'''Привет!  Добро пожаловать в "Советчик по выбору карьеры"!
+Я помогу тебе найти подходящую профессию, исходя из твоих интересов, навыков и предпочтений. 
+📌 Как я могу помочь?
+🔹 Предложу профессии, которые подойдут тебе
+🔹 Подскажу, какие навыки развивать
+🔹 Дам советы по обучению и поиску работы''')
+@bot.message_handler(commands=['help'])
+def handle_start(message):
+    bot.send_message(message.chat.id,'ТУТ НУЖНО НАПИСАТЬ КОМАНДЫ КОТОРЫЕ У БОТА БУДУТ')
+@bot.message_handler(commands=['add'])
+def adduser(message):
+    data = message.text.split('/add')[1].strip().split()
+    TG.add_user(message.chat.id,data[0],data[1],data[2])
+
+
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
---
+++ logic.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+class CareerAdvisorBot:
+    def __init__(self, db_name="career_bot.db"):
+        self.db_name = db_name
+        self.create_database()
+
+    def create_database(self):
+        """Создает таблицу пользователей и предпочтений, если она не существует"""
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER UNIQUE,
+                    name TEXT,
+                    age INTEGER,
+                    interests TEXT
+                )
+            ''')
+            conn.commit()
+
+    def add_user(self, user_id, name, age, interests):
+        """Добавляет нового пользователя в базу данных"""
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT OR REPLACE INTO users (user_id, name, age, interests) 
+                VALUES (?, ?, ?, ?)''', (user_id, name, age, interests))
+            conn.commit()
+
+    def get_user(self, user_id):
+        """Получает информацию о пользователе"""
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+            return cursor.fetchone()
+        
+if __name__ == "__main__":
+ bot = CareerAdvisorBot()
